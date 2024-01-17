5136299d631d7594fca0c5e7f567db56f2dc2f9e
F0rno <pablo.fornell.perinan@gmail.com>
2024-01-17 15:47:10 +0100

    internal(API): Root / now responds with server status, always running if active

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
 
-@Controller('')
+@Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
   @Get()
-  getHello(): any {
-    return this.appService.getHello();
+  getRoot(): any {
+    return { status: 'Running', message: 'Welcome to the Faro API' };
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
@@ -14,6 +13,6 @@ import { ConfigModule } from '@nestjs/config';
     ConfigModule.forRoot({ envFilePath: `${process.env.NODE_ENV}.env` }),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
