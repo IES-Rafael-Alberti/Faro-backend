e927f1980ac58c7faf400eb9b4384ee6220a5af7
F0rno <pfornell@iesrafaelalberti.es>
2024-03-11 12:33:14 +0100

    internal: restoring the proyect to start

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,9 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-import { UserModule } from './user/user.module';
 import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      process.env.MONGO_URI || 'mongodb://localhost:27017/faro',
-    ),
-    UserModule,
     ConfigModule.forRoot({ envFilePath: `${process.env.NODE_ENV}.env` }),
   ],
   controllers: [AppController],
