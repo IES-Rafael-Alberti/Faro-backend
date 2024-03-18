2d4f490e896200372adaf5a67470609d1c2b15f2
F0rno <pfornell@iesrafaelalberti.es>
2024-03-18 12:33:36 +0100

    test: remove test of main

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+// TODO: Add a pipe to validate the request body
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
