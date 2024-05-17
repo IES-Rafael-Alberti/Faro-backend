b6ddb27199043c268914bde6d5d1b076c520a91e
F0rno <pfornell@iesrafaelalberti.es>
2024-05-17 10:23:41 +0200

    feat: user impersonation protection added

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,28 +1,43 @@
-import { Controller, Get, Body, Param, Delete, Patch } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Body,
+  Param,
+  Delete,
+  Patch,
+  UseGuards,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UserDto } from './entities/user.dto';
 import { InputUserDto } from './entities/input.user.dto';
-// TODO: Assert tha the user is the only who can modify his own data
+import { AuthGuard } from 'src/auth/guards/auth.guard';
+import { UserImpersonationProtectionGuard } from 'src/auth/guards/UserImpersonationProtectionGuard.guard';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @UseGuards(AuthGuard)
   @Get()
   findAll(): Promise<UserDto[]> {
     return this.usersService.findAll();
   }
 
+  @UseGuards(AuthGuard)
   @Get(':id')
   findOne(@Param('id') id: string): Promise<UserDto | null> {
     return this.usersService.findOneByIdUserDto(id);
   }
 
+  @UseGuards(AuthGuard)
+  @UseGuards(UserImpersonationProtectionGuard)
   @Patch('update')
   updateById(@Body() userDto: InputUserDto): Promise<UserDto> {
     return this.usersService.path(userDto);
   }
 
+  @UseGuards(AuthGuard)
+  @UseGuards(UserImpersonationProtectionGuard)
   @Delete(':id')
   remove(@Param('id') id: string): Promise<void> {
     return this.usersService.remove(id);
