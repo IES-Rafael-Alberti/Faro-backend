1f5338118a7ad98dbe250b6bb0454ced1f763dd9
F0rno <pfornell@iesrafaelalberti.es>
2024-06-05 20:28:49 +0200

    feat: added guards to likes

--- src/modules/publications/likes/likes.controller.ts
+++ src/modules/publications/likes/likes.controller.ts
@@ -6,14 +6,19 @@ import {
   Body,
   Param,
   HttpException,
+  UseGuards,
 } from '@nestjs/common';
 import { LikesService } from './likes.service';
 import { CreateLikeDto } from './entities/like.dto';
+import { AuthGuard } from 'src/auth/guards/auth.guard';
+import { UserImpersonationProtectionGuard } from 'src/auth/guards/UserImpersonationProtectionGuard.guard';
 
 @Controller('likes')
 export class LikesController {
   constructor(private readonly likesService: LikesService) {}
 
+  @UseGuards(AuthGuard)
+  @UseGuards(UserImpersonationProtectionGuard)
   @Post()
   async addLike(@Body() createLikeDto: CreateLikeDto): Promise<CreateLikeDto> {
     try {
@@ -26,6 +31,8 @@ export class LikesController {
     }
   }
 
+  @UseGuards(AuthGuard)
+  @UseGuards(UserImpersonationProtectionGuard)
   @Get(':publication_id')
   async findAllLikesByPublicationId(
     @Param('publication_id') publication_id: string,
@@ -33,6 +40,8 @@ export class LikesController {
     return await this.likesService.findAllLikesByPublicationId(publication_id);
   }
 
+  @UseGuards(AuthGuard)
+  @UseGuards(UserImpersonationProtectionGuard)
   @Delete()
   async removeLike(@Body() createLikeDto: CreateLikeDto): Promise<void> {
     try {
