719bc8fb76c79caa5d1d50c69eb5e6ad1a942893
F0rno <pfornell@iesrafaelalberti.es>
2024-06-18 13:52:03 +0200

    docs: auth

--- src/auth/guards/BaseImpersonationProtectionGuard.guard.ts
+++ src/auth/guards/BaseImpersonationProtectionGuard.guard.ts
@@ -11,8 +11,17 @@ import { Request } from 'express';
 export abstract class BaseImpersonationProtectionGuard implements CanActivate {
   constructor(protected jwtService: JwtService) {}
 
+  /**
+   * An array of fields to check in the request. These fields are used to determine if a user is trying to access another user's data.
+   */
   abstract fieldsToCheck: string[];
 
+  /**
+   * Determines whether the current user can activate the route or not.
+   * @param {ExecutionContext} context - The execution context.
+   * @returns {Promise<boolean>} - A promise that resolves to a boolean indicating whether the user can activate the route or not.
+   * @throws {UnauthorizedException} - Throws an exception if the user is not authorized.
+   */
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
     const token = this.extractTokenFromHeader(request);
@@ -52,11 +61,23 @@ export abstract class BaseImpersonationProtectionGuard implements CanActivate {
     return true;
   }
 
+  /**
+   * Extracts the token from the request header.
+   * @param {Request} request - The request object.
+   * @returns {string | undefined} - The extracted token or undefined if the token is not found.
+   */
   protected extractTokenFromHeader(request: Request): string | undefined {
     const [type, token] = request.headers.authorization?.split(' ') ?? [];
     return type === 'Bearer' ? token : undefined;
   }
 
+  /**
+   * Checks the parameters from the URL against the user's session.
+   * @param {string[]} fieldsToCheck - The fields to check.
+   * @param {Request} request - The request object.
+   * @param {any} userSesion - The user's session.
+   * @returns {boolean} - Returns true if the parameters match the user's session, false otherwise.
+   */
   protected checkParametersFromURL(
     fieldsToCheck: string[],
     request: Request,
@@ -75,6 +96,13 @@ export abstract class BaseImpersonationProtectionGuard implements CanActivate {
     return valid;
   }
 
+  /**
+   * Checks the parameters from the body against the user's session.
+   * @param {string[]} fieldsToCheck - The fields to check.
+   * @param {Request} request - The request object.
+   * @param {any} userSesion - The user's session.
+   * @returns {boolean} - Returns true if the parameters match the user's session, false otherwise.
+   */
   protected checkParametersFromBody(
     fieldsToCheck: string[],
     request: Request,
