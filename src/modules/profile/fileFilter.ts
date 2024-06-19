38f90465d0c09be724cb3dd3f8d82b503f875dfc
JuanjoSG5 <113671548+JuanjoSG5@users.noreply.github.com>
2024-06-19 19:33:45 +0200

    fix: fixed an issue with the image upload

--- src/modules/profile/fileFilter.ts
+++ src/modules/profile/fileFilter.ts
@@ -5,7 +5,7 @@ export const imageFileFilter = (
   file: Express.Multer.File,
   callback: (error: Error | null, acceptFile: boolean) => void,
 ) => {
-  if (!file.mimetype.match(/\/(jpg|jpeg|png|gif|webp)$/)) {
+  if (!file.mimetype.match(/\/(jpg|jpeg|png|gif|webp|bin)$/)) {
     return callback(new Error('Only image files are allowed!'), false);
   }
   callback(null, true);
